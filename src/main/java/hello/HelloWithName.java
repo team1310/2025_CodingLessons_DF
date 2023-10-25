752bdb4860fb903155b2cfa9fe335fc2d0eaeba3
Richard McMullin <richard.e.mcmullin@gmail.com>
2023-10-25 11:49:14 -0400

    Hello World

---
+++ src/main/java/hello/HelloWithName.java
@@ -0,0 +1,19 @@
+package main.java.hello;
+
+import java.util.Scanner;
+
+public class HelloWithName {
+
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] varArgs) {
+
+        System.out.println("What is your name?");
+
+        String name = scanner.nextLine();
+
+        System.out.println("Hello " + name);
+
+    }
+
+}
---
+++ src/main/java/hello/HelloWorld.java
@@ -0,0 +1,11 @@
+package main.java.hello;
+
+public class HelloWorld {
+
+    public static void main(String[] varArgs) {
+
+        System.out.println("Hello World");
+
+    }
+
+}
